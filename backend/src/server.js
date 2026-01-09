e81abe2568512e92b234371c05ed13c0805139ce
rajaNayak123 <nayakraja151@gmail.com>
2026-01-09 12:09:21 +0530

    complete basic setup

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const app = express();
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`server started on port ${process.env.PORT}`);
+});

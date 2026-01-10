cee75f0609ce562e6a27f899a1e46793398a1e6c
rajaNayak123 <nayakraja151@gmail.com>
2026-01-10 11:18:45 +0530

    implement middleware for authentication

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+
+const auth = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies.token || req.headers["authorization"]?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ error: "No token provided" });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decodedToken.userId).select("username");
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+export{auth}

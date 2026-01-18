db1e766bac6116b5fe35620b6dbb9cbf7a51ecca
rajaNayak123 <nayakraja151@gmail.com>
2026-01-18 20:27:32 +0530

    import user route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {connectToDB} from "./config/database.js"
 
 dotenv.config(); 
 
+// imports the routes
+import { userRouter } from "../src/routes/userRoutes.js"
+
 const app = express();
+connectToDB()
 
-
+app.use("api/user", userRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`server started on port ${process.env.PORT}`);

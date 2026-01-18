d3f19fb67173ab241ddc46c549438be534732517
rajaNayak123 <nayakraja151@gmail.com>
2026-01-18 21:00:38 +0530

    define the routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,13 @@ dotenv.config();
 
 // imports the routes
 import { userRouter } from "../src/routes/userRoutes.js"
+import { roomRouter } from './routes/roomRoutes.js';
 
 const app = express();
 connectToDB()
 
 app.use("api/user", userRouter)
+app.use("api/room", roomRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`server started on port ${process.env.PORT}`);

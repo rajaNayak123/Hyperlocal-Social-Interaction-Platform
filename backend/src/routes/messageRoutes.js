22ee08889a1234587de28488b9e7a8790e7fec19
rajaNayak123 <nayakraja151@gmail.com>
2026-01-19 08:42:43 +0530

    add rate limit for the send message

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { sendMessage, getMessage } from "../controllers/messageController.js"
 import { auth } from "../middleware/auth.js"
+import { messageLimiter } from "../middleware/rateLimit.js"
 
 const router = express.Router()
 
-router.post("/", auth, sendMessage)
+router.post("/", auth, messageLimiter, sendMessage)
 router.get("/room/:roomId", auth, getMessage)
 
 export {messageRoute}
